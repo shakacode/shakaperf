756104701c55bd31d00e46340bf63d2ff3535a5c
Ramez Weissa <ramez@shakacode.com>
2026-02-05 15:04:44 +0200

    feat: update node.js version

---
+++ demo-ecommerce/twin-servers.config.ts
@@ -0,0 +1,25 @@
+import { defineConfig } from 'shaka-twin-servers';
+
+export default defineConfig({
+  projectDir: '.',
+  controlDir: '../../shaka-perf-control/demo-ecommerce',
+  dockerBuildDir: '..',
+  dockerBuildArgs: {
+    RUBY_VERSION: '3.3.7',
+    NODE_VERSION: '24.13.0',
+  },
+  composeFile: 'docker-compose.yml',
+  procfile: 'Procfile',
+  stopSignals: {
+    'experiment-rails': 'TERM',
+    'control-rails': 'TERM',
+  },
+  images: {
+    control: 'demo-ecommerce:control',
+    experiment: 'demo-ecommerce:experiment',
+  },
+  volumes: {
+    control: '~/demo_ecommerce_control_docker_volume',
+    experiment: '~/demo_ecommerce_experiment_docker_volume',
+  },
+});
