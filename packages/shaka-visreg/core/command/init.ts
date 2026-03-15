619a439dff3e199edc9713121da07d5152057fa1
Ramez Weissa <ramez@shakacode.com>
2026-03-15 13:52:27 +0200

    feat: update init command to create default config, abtest

--- packages/shaka-visreg/core/command/init.ts
+++ packages/shaka-visreg/core/command/init.ts
@@ -1,3 +1,4 @@
+import path from 'node:path';
 import { copy } from 'fs-extra';
 import createLogger from '../util/logger.js';
 import type { RuntimeConfig } from '../types.js';
@@ -5,22 +6,30 @@ import type { RuntimeConfig } from '../types.js';
 const logger = createLogger('init');
 
 /**
- * Copies a boilerplate config file to the current config file location.
+ * Copies boilerplate config, engine scripts, and ab-tests template to the project directory.
  */
 export function execute (config: RuntimeConfig) {
   const promises = [];
+
   if (config.engine_scripts) {
     logger.log("Copying '" + config.engine_scripts_default + "' to '" + config.engine_scripts + "'");
     promises.push(copy(config.engine_scripts_default, config.engine_scripts));
   } else {
-    logger.error('ERROR: Can\'t generate a scripts directory. No \'engine_scripts\' path property was found in visreg.json.');
+    logger.error('ERROR: Can\'t generate a scripts directory. No \'engine_scripts\' path property was found.');
   }
 
-  // Copies a boilerplate config file to the current config file location.
+  // Copy boilerplate config file
   promises.push(copy(config.captureConfigFileNameDefault, config.configFileName).then(function () {
     logger.log("Configuration file written at '" + config.configFileName + "'");
   }, function (err: unknown) {
     throw err;
+  }));
+
+  // Copy ab-tests template
+  const abTestsSrc = path.join(config.visregRoot, 'capture', 'ab-tests');
+  const abTestsDest = path.join(config.projectPath, 'ab-tests');
+  promises.push(copy(abTestsSrc, abTestsDest).then(function () {
+    logger.log("Test templates written at '" + abTestsDest + "'");
   }));
 
   return Promise.all(promises);
