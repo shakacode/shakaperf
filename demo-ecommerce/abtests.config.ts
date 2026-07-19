3c79cb391f81becd726410ab8c46719bf34ff808
Ramez Weissa <ramez@shakacode.com>
2026-07-19 20:07:27 +0300

    feat: add rebuild commands to config

--- demo-ecommerce/abtests.config.ts
+++ demo-ecommerce/abtests.config.ts
@@ -85,6 +85,19 @@ export default defineConfig({
     lighthouseConfig: LIGHTHOUSE_CONFIG,
   },
 
+  bisect: {
+    rebuildCommands: [
+      {
+        description: 'Install JavaScript dependencies',
+        command: 'yarn install --immutable',
+      },
+      {
+        description: 'Precompile application assets',
+        command: 'rm -rf public/packs tmp/cache && SECRET_KEY_BASE_DUMMY=1 ./bin/rails assets:precompile',
+      },
+    ],
+  },
+
   twinServers: {
     experimentDir: process.cwd(),
     controlDir: process.env.CONTROL_REPO_DIR || '../../shaka-perf-control/demo-ecommerce',
